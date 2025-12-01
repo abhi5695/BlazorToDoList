a5aa229c0bbb84b1bbd34f3bc7d0cd44489844f4
Abhijith Bhaskaran <Abhijithbhaskarank@gmail.com>
2025-12-01 17:00:50 +0530

    Add project files.

---
+++ BlazorToDoList/Components/Models/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BlazorToDoList.Components.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CompletedDateTime { get; set; }
+    }
+}
---
+++ BlazorToDoList/Components/Models/ToDoRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace BlazorToDoList.Components.Models
+{
+    public class ToDoRepository
+    {
+        private static List<ToDoItem> ToDoList = new List<ToDoItem>
+        {
+            new ToDoItem{Id=1, Title="New Item", IsCompleted=false}
+        };
+        public static List<ToDoItem> GetToDos() => ToDoList.OrderByDescending(s=>s.Id).OrderBy(s => s.IsCompleted).ToList();
+
+        public static void AddToDo(ToDoItem toDo)
+        {
+            int maxId = ToDoList.Max(t => t.Id);
+            toDo.Id = maxId + 1;
+            ToDoList.Add(toDo);
+        }
+        public static void UpdateToDo(int id, ToDoItem toDo)
+        {
+            if (id != toDo.Id) return;
+
+            ToDoItem updatedToDo = ToDoList.FirstOrDefault(t=>t.Id == id);
+            if (updatedToDo != null)
+            {
+                updatedToDo.Title = toDo.Title;
+                updatedToDo.IsCompleted = toDo.IsCompleted;
+                updatedToDo.CompletedDateTime=DateTime.Now;
+            }
+        }
+    }
+}
